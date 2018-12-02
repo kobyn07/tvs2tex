55e58edc6948bf79583c0137a663a6ff6a5d42e7
kobyn <mail@kobyn.net>
2018-12-02 21:10:43 +0900

    表示を調整

--- tvs2tex.py
+++ tvs2tex.py
@@ -9,27 +9,27 @@ class Application(tk.Frame):
         self.master.geometry('1000x500')
         # caption の入力
         captitle = tk.Label(self, text=u'表タイトル')
-        captitle.grid(row=0, column=0)
+        captitle.grid(row=0, column=0, sticky=tk.E)
         capbox = tk.Entry(self, width=30)
         capbox.grid(row=0, column=1, padx=10, pady=10)
 
         # label の入力
         labtitle = tk.Label(self, text=u'ラベル')
-        labtitle.grid(row=1, column=0)
+        labtitle.grid(row=1, column=0, sticky=tk.E)
         labbox = tk.Entry(self, width=30)
         labbox.grid(row=1, column=1)
 
         # 表の入力
         intitle = tk.Label(self, text='入力')
-        intitle.grid(row=2, column=0, padx=10, pady=15)
+        intitle.grid(row=2, column=0, padx=10, pady=10)
         inbox = tk.Text(self, width=50)
         inbox.grid(row=3, column=0, padx=10)
 
         # 表の出力
         outtitle = tk.Label(self, text='出力')
-        outtitle.grid(row=2, column=3, padx=30, pady=15)
+        outtitle.grid(row=2, column=2, padx=30, pady=10)
         outbox = tk.Text(self, width=50)
-        outbox.grid(row=3, column=3, padx=10)
+        outbox.grid(row=3, column=2, padx=10)
 
 
 
